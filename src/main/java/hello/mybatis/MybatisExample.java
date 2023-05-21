eb3ddfce0d756c5a6b02a24e023c633447d62d17
gyoon <xmas6904201@gmail.com>
2023-05-22 08:40:28 +0900

    profile을 이용하여 test코드를 이용할 시에 메모리 db를 이용하는 방법 실험완료 이유 : 현재 우리 지역의 인터넷 공사중이어서 혹시나 안될 경우를 데비하여 공부할 겸 임베디드 모드를 설정해두었다. test코드에 사용할 Datasource @Bean 직접 구현

--- src/main/java/hello/mybatis/MybatisExample.java
+++ src/main/java/hello/mybatis/MybatisExample.java
@@ -2,16 +2,21 @@ package hello.mybatis;
 
 import hello.mybatis.config.*;
 import hello.mybatis.repository.ItemRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateConfigV1.class)
 //@Import(JdbcTemplateConfigV2.class)
+@Log4j2
 @Import(JdbcTemplateConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.mybatis.web")
 public class MybatisExample {
@@ -26,4 +31,23 @@ public class MybatisExample {
         return new TestDataInit(itemRepository);
     }
 
+
+    /**
+     * Profile이 test인 경우 => application.properties에 spring.profiles.active=test => 즉 테스트 코드를 실행하는 경우 아래와 같이 <br><br>
+     * Datasource Bean을 직접 커스텀하여 컨테이너가 관리하도록 할 것이다.<br><br>
+     * 커스텀이라고 말한 이유 : DataSource 와 TranscationManager는 스프링이 컨테이너가 스스로관리하여 주입이 필요한 경우 알아서 주입을해준다.
+     *
+     * @return dataSource
+     */
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        log.info("메모리 데이터 베이스 초기화");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
 }

cb605a00ecc4be6224bf32201e445040fe2e1a09
gyoon <xmas6904201@gmail.com>
2023-05-22 02:04:58 +0900

    기존 코드 주석 처리

--- src/main/java/hello/mybatis/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
+++ src/main/java/hello/mybatis/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -50,6 +50,15 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
     }
 
 
+    /**
+     * SqlParameterSource source = new BeanPropertySqlParameterSource(item) <br><br>
+     *
+     * key = itemName <br><br>
+     * value = [itemName의 실제 값]으로 해준다.
+     * @param item
+     * @return
+     */
+
     @Override
     public Item save(Item item) {
         // id는 db가 직접 생성 때문에 id값을 개발자가 지정해주지 않아도 된다.
@@ -65,6 +74,16 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
         item.setId(key);
         return item;
     }
+
+    /**
+     * 아래의 경우 SqlParameterSource parameter = new BeanPropertySqlParameterSource()를 사용못한다. <br>
+     * 이유 : BeanProperty 말 그대로 해당 객체의 필드의 값을 가져와서 사용하는 것이다. <br><br>
+     * 하지만 아래의 메소드의 경우에는 id값도 설정해주어야한는대 ItemUpdateDTO의 BeanProperty를 확인하면 어디에도 Id Propertry가 존재하지 않는다.<br><br>
+     * 때문에 Map.of 혹은 MapSqlParameterSource를 사용 할 수 밖에 없다.<br><br>
+     *
+     * @param itemId
+     * @param updateParam
+     */
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
@@ -87,6 +106,14 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
         try {
             // 변경사항 NamedParmeterJdbcTemplate 방법 3
             Map<String, Object> param = Map.of("id", id);
+
+            // 자바 9부터 도입된 Map.of 비슷한 예제로 List.of로 리스트를 new 연산자를 사용하지 않고 만들수가 있다.
+            // 하지만 주의할점이 2가지가 있다.
+            // 첫번째 immutable 하다. 다시말해 put 혹은 remove와 같은 메소드로 객체의 데이터를 변경할 수 없게 된다는 것이다.
+            // 두번 째 기존 new 연산자를 사용하여 JCF의 인스터를 생성할 때 생성자에 파라미터값을 넣어줄 수가 있는대 이때 선언하지 않는 경우 기본 크기가 10으로 지정된다.
+            // 그런데 Map.of , List.of등과 같이 JCF를 초기화하는 작업을 하면 기본크기가 10으로 고정이된다.
+
+            // Map의 경우 Map.entry를 사용한다면 size가 10이상으로 Map을 초기화 할 수가 있다.
 
             Item item = this.template.queryForObject(sql, param, this.itemRowMapper());
             return Optional.of(item);
@@ -130,5 +157,6 @@ public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
 
     private RowMapper<Item> itemRowMapper() {
         return BeanPropertyRowMapper.newInstance(Item.class);
+        // 정리하자면 BeanPropertyRowMapper는 ResultSet의 결과를 받아서 자바 빈 규약에 맞추어 데이터를 변환한다.
     }
 }
